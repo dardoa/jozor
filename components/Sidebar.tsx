41222e12f89aa1d27b1164bb832ee13f82deb5f8
[dyad] <git@dyad.sh>
2025-12-04 14:14:00 +0300

    [dyad] دمج مكون SidebarActions في Sidebar.tsx - wrote 1 file(s)

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { BioTab } from './sidebar/BioTab';
 import { MediaTab } from './sidebar/MediaTab';
 import { SidebarFooter } from './sidebar/SidebarFooter';
 import { SidebarTabs } from './sidebar/SidebarTabs';
+import { SidebarActions } from './sidebar/SidebarActions'; // New import
 import { getTranslation } from '../utils/translations';
 import { Person, Language, UserProfile, FamilyActionsProps } from '../types';
 import { useTranslation } from '../context/TranslationContext'; // Import useTranslation
@@ -91,11 +92,13 @@ export const Sidebar: React.FC<SidebarProps> = memo(({
                 {activeTab === 'media' && <MediaTab person={person} isEditing={isEditing} onUpdate={onUpdate} user={user} />}
             </div>
             
+            {/* Sidebar Actions (Quick Add Speed Dial) */}
+            <SidebarActions familyActions={familyActions} /> {/* New: Render SidebarActions here */}
+
             <SidebarFooter 
                 person={person} isEditing={isEditing} setIsEditing={setIsEditing}
                 onDelete={onDelete}
-                familyActions={familyActions} // Pass new grouped prop
-                // Removed t={t}
+                // Removed familyActions as it's no longer used directly in this component
             />
         </div>
     </>
